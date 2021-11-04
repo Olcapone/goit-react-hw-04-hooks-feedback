8914e75758ff9463e9b1bc9580a59cafa0bd9b1d
Olga Ponomarenko <oip@anadeainc.com>
2021-11-04 16:42:42 +0200

    unit

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,5 +1,5 @@
 import s from "./Notification.module.css";
 
-const Notification = ({ message }) => <h2 className={s.title}>{message}</h2>;
+const Notification = ({ message }) => (<h2 className={s.title}>{message}</h2>);
 
 export default Notification;
